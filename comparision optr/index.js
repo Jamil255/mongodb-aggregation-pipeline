e110a340cfeb538b5e9e35234cb4838c98cc5b30
JAMIL AFZAL <134855468+Jamil255@users.noreply.github.com>
2024-07-23 21:36:21 +0500

    Update index.js

--- comparision optr/index.js
+++ comparision optr/index.js
@@ -28,7 +28,7 @@ export const getUserDataFun = async (req, res) => {
     const eq = await userModel.find({age:{$eq:21}})
     const ne = await userModel.find({age:{$ne:21}})
     const lte = await userModel.find({age:{$lte:90}})
-    const gte = await userModel.find({gender:{$in:["female"]}})
+    const gte = await userModel.find({gender:{$in:["male"]}})
     const data = await userModel.find({gender:{$nin:["female"]}})
     res.json(data)
   } catch (error) {
